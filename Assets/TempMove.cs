f265f7f8924859b6cab51244bf8dead24b807b34
HypnoticSheep <mike.keck@yahoo.com>
2017-05-08 21:24:34 -0400

    Added Axeman
    And movement. But mostly Axeman.

---
+++ Assets/TempMove.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempMove : MonoBehaviour {
+
+	public float xMove;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.D)) {
+			transform.Translate (new Vector2(xMove,0));
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			transform.Translate (new Vector2(-1*xMove,0));
+		}
+	}
+}
