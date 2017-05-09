a875824edb0bb8a74a0c4b2b17949f56b4b364ee
HypnoticSheep <mike.keck@yahoo.com>
2017-05-08 20:44:38 -0400

    Adding All

---
+++ Assets/Button.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour {
+
+
+	public Color defaultColor;
+	public Color selectedColor;
+	private Material mat;
+	private Renderer rend;
+
+	void Start(){
+		rend = GetComponent<Renderer> ();
+		mat = rend.material;
+	}
+
+	void OnTouchDown(){
+		mat.color = selectedColor;
+	}
+
+	void OnTouchUp(){
+		mat.color = defaultColor;
+	}
+
+	void OnTouchStay(){
+		mat.color = selectedColor;
+	}
+
+	void OnTouchDrag(){
+		mat.color = selectedColor;
+	}
+
+	void OnTouchExit(){
+		mat.color = defaultColor;
+	}
+}
